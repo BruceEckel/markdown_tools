a55137684534fececbd7b530eb49d5ab0fab187d
Bruce Eckel <mindviewinc@gmail.com>
2023-11-17 23:44:02 -0700

    changed help flag to -h

--- src/markdown_tools/tools.py
+++ src/markdown_tools/tools.py
@@ -13,7 +13,10 @@ from markdown_tools import (
 )
 from pathlib import Path
 
-app = typer.Typer(no_args_is_help=True)
+app = typer.Typer(
+    no_args_is_help=True,
+    context_settings={"help_option_names": ["-h"]},
+)
 
 
 @app.command()
