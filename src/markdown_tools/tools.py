a453a5a7ea4e79dda013bbcaca86e44928647568
Bruce Eckel <mindviewinc@gmail.com>
2023-11-17 23:36:50 -0700

    Update tools.py
    context_settings={"help_option_names": [""]} DOES NOT work when no_args_is_help=True

--- src/markdown_tools/tools.py
+++ src/markdown_tools/tools.py
@@ -13,10 +13,7 @@ from markdown_tools import (
 )
 from pathlib import Path
 
-app = typer.Typer(
-    no_args_is_help=True,
-    context_settings={"help_option_names": [""]},
-)
+app = typer.Typer(no_args_is_help=True)
 
 
 @app.command()
