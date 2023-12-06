99df2c118dbda8e150df9e7d45d58edbd49698bc
Bruce Eckel <mindviewinc@gmail.com>
2023-12-05 18:33:01 -0700

    Update update_listings.py

--- src/markdown_tools/update_listings.py
+++ src/markdown_tools/update_listings.py
@@ -22,24 +22,25 @@ def compare_listings_to_source_files(md: Path):
         assert (
             full_path.exists()
         ), f"ERROR: {full_path.as_posix()} does not exist"
-        # console.print(f"{full_path.as_posix()}", end=": ")
         source_file = SourceCode.from_source_file(full_path)
         diff = compare_strings(source_code.code, source_file.code)
         match diff.result:
             case DiffResult.NONE:
                 assert source_file == source_code
-                p = Panel(
-                    Text(full_path.as_posix(), style="green"),
-                    title="[turquoise2 bold][MATCH][/ turquoise2 bold]",
-                    title_align="left",
-                )
-                console.print(p)
                 console.print(
-                    f"[turquoise2 bold][MATCH][/ turquoise2 bold]: {full_path.as_posix()}"
+                    Panel(
+                        Text(full_path.as_posix(), style="green"),
+                        title="[turquoise2 bold][MATCH]",
+                        title_align="left",
+                    )
                 )
             case DiffResult.BLANK_LINES:
                 console.print(
-                    f"[bright_blue bold][BLANK LINES ONLY CHANGED][/ bright_blue bold]: {full_path.as_posix()}"
+                    Panel(
+                        Text(full_path.as_posix(), style="green"),
+                        title="[bright_blue bold][BLANK LINES ONLY CHANGED]",
+                        title_align="left",
+                    )
                 )
                 console.print(
                     Panel(
@@ -49,7 +50,11 @@ def compare_listings_to_source_files(md: Path):
                 )
             case DiffResult.CONTENT:
                 console.print(
-                    f"[bright_red][CONTENT DIFFERENCE][/ bright_red]: {full_path.as_posix()}"
+                    Panel(
+                        Text(full_path.as_posix(), style="green"),
+                        title="[bright_red][CONTENT DIFFERENCE]",
+                        title_align="left",
+                    )
                 )
                 console.print(
                     Panel(
@@ -57,8 +62,6 @@ def compare_listings_to_source_files(md: Path):
                         title="Diff: Markdown <-> Source",
                     )
                 )
-                console.print("In Markdown:", source_code)
-                console.print("Source code file:", source_file)
 
 
 def update_listings(md: Path):
@@ -78,19 +81,19 @@ def update_listings(md: Path):
             # 2. md_file[md_file.index_of(source_code)] = new_source_code
 
 
-def check_code_block(scl: SourceCode) -> str | None:
-    if scl.language_name == "text" or scl.ignore:
-        return None
-    return f"{scl.language_name}: {scl.source_file_name}"
+# def check_code_block(scl: SourceCode) -> str | None:
+#     if scl.language_name == "text" or scl.ignore:
+#         return None
+#     return f"{scl.language_name}: {scl.source_file_name}"
 
 
-def check_code_listings(md: Path):
-    md_file = MarkdownFile(md)
-    for listing in md_file.code_listings():
-        assert isinstance(listing, SourceCode)
-        if r := check_code_block(listing):
-            md_file.display_name_once()
-            console.print(r)
+# def check_code_listings(md: Path):
+#     md_file = MarkdownFile(md)
+#     for listing in md_file.code_listings():
+#         assert isinstance(listing, SourceCode)
+#         if r := check_code_block(listing):
+#             md_file.display_name_once()
+#             console.print(r)
 
 
 def display_markdown_comments(md: Path):
