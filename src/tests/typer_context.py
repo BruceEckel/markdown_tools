45d979dc0874d651216ca80675fca2e178632c6f
Bruce Eckel <mindviewinc@gmail.com>
2023-12-14 19:36:14 -0700

    Update typer_context.py

--- src/tests/typer_context.py
+++ src/tests/typer_context.py
@@ -1,23 +1,31 @@
 import typer
+import readchar
 
 app = typer.Typer(
     no_args_is_help=True,
     add_completion=False,
     context_settings={"help_option_names": ["-h"]},
     rich_markup_mode="rich",
+    epilog="Shows up after the help message",
 )
 
 
 @app.command("1")
-def create(username: str):
-    "Create user"
-    print(f"Creating user: {username}")
+def one():
+    "Command One"
+    print("This is Command One!")
 
 
 @app.command("2")
-def delete(username: str):
-    "Delete user"
-    print(f"Deleting user: {username}")
+def two():
+    "Command Two"
+    print("This is Command Two!")
+
+
+@app.command("m")
+def menu(ctx: typer.Context):
+    "Menu Mode"
+    print("This is Menu Mode!")
 
 
 @app.callback()
@@ -25,7 +33,16 @@ def main(ctx: typer.Context):
     """
     Manage users in the awesome CLI app.
     """
-    print(f"About to execute command: {ctx.invoked_subcommand}")
+    if ctx.invoked_subcommand == "m":
+        print(ctx.get_help())
+        while (key := readchar.readchar()) not in "12":
+            print(key)
+        print(f"You selected {key}")
+        match key:
+            case "1":
+                ctx.call_on_close(one)
+            case "2":
+                ctx.call_on_close(two)
 
 
 if __name__ == "__main__":
